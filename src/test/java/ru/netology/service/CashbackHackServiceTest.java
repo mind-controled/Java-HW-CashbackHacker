f214e33d23f14762157d7e0197996714d8e9087d
mind_control <mind_control@mail.ru>
2020-05-19 17:43:49 +0300

    Few mistakes fixed

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,38 +1,32 @@
 package ru.netology.service;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
 
-    @Test
+    @org.junit.Test
     public void shouldReturn1000for0() {
-        CashbackHackService service = new CashbackHackService();
         assertEquals(1000, service.remain(0));
     }
 
-    @Test
+    @org.junit.Test
     public void shouldReturn100For900() {
-        CashbackHackService service = new CashbackHackService();
         assertEquals(900, service.remain(100));
     }
 
-    @Test
+    @org.junit.Test
     public void shouldReturn999For1() {
-        CashbackHackService service = new CashbackHackService();
         assertEquals(999, service.remain(1));
     }
 
-    @Test
+    @org.junit.Test
     public void shouldReturn999For1001() {
-        CashbackHackService service = new CashbackHackService();
         assertEquals(999, service.remain(1001));
     }
 
-    @Test
+    @org.junit.Test
     public void shouldReturn0For1000() {
-        CashbackHackService service = new CashbackHackService();
         assertEquals(0, service.remain(1000));
     }
 }
